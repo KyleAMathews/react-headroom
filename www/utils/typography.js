b6346f758a16c72c4faeee3c29a5fc69b4307c7a
Kyle Mathews <mathews.kyle@gmail.com>
2016-09-24 00:32:24 -0700

    Switch to use palantino

--- www/utils/typography.js
+++ www/utils/typography.js
@@ -1,7 +1,11 @@
 import Typography from 'typography'
 import theme from 'typography-theme-noriega'
 import CodePlugin from 'typography-plugin-code'
+
 theme.plugins = [new CodePlugin()]
+theme.bodyFontFamily = ['Palatino', 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', 'Georgia', 'serif']
+theme.headerFontFamily = ['Palatino', 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', 'Georgia', 'serif']
+theme.googleFonts = []
 
 const typography = new Typography(theme)
 
