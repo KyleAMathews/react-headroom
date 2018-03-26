c34c27875d55f637410b9f00c0d19124f7f2c5cc
Hernan Garchtrom <hernang87@gmail.com>
2018-03-26 09:53:02 -0300

    Added validations for getElementHeight, getElementPhysicalHeight

--- src/index.js
+++ src/index.js
@@ -133,16 +133,31 @@ export default class Headroom extends Component {
     )
   }
 
-  getElementPhysicalHeight = elm => Math.max(
-    elm.offsetHeight,
-    elm.clientHeight
-  )
+  getElementPhysicalHeight = elm => {
+    let offsetHeight = 0
+    let clientHeight = 0;
 
-  getElementHeight = elm => Math.max(
-    elm.scrollHeight,
-    elm.offsetHeight,
-    elm.clientHeight,
-  )
+    if (elm) {
+      offsetHeight = elm.offsetHeight;
+      clientHeight = elm.clientHeight;
+    }
+
+    return Math.max(offsetHeight, clientHeight);
+  };
+
+  getElementHeight = elm => {
+    let offsetHeight = 0;
+    let clientHeight = 0;
+    let scrollHeight = 0;
+
+    if (elm) {
+      offsetHeight = elm.offsetHeight;
+      clientHeight = elm.clientHeight;
+      scrollHeight = elm.scrollHeight;
+    }
+
+    return Math.max(scrollHeight, offsetHeight, clientHeight);
+  };
 
   getScrollerPhysicalHeight = () => {
     const parent = this.props.parent()
