1ddd58a39c4899e72afa81c82910dc2fcf7ac1ad
benhakimilyass <ilyassbenhakim2@gmail.com>
2020-09-12 16:24:16 +0100

    A 300 B react custome hook that returns the state of the nav (if it's shown or not)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react' // eslint-disable-line import/no-unresolved
-import PropTypes from 'prop-types'
-import shallowequal from 'shallowequal'
-import raf from 'raf'
-import shouldUpdate from './shouldUpdate'
+import React, { Component } from "react"; // eslint-disable-line import/no-unresolved
+import PropTypes from "prop-types";
+import shallowequal from "shallowequal";
+import raf from "raf";
+import shouldUpdate from "./shouldUpdate";
 
-const noop = () => {}
-
+const noop = () => {};
+export { default as useHeadroom } from "./useHeadroom";
 export default class Headroom extends Component {
   static propTypes = {
     className: PropTypes.string,
@@ -38,214 +38,215 @@ export default class Headroom extends Component {
     calcHeightOnResize: true,
   };
 
-  static getDerivedStateFromProps (props, state) {
-    if (props.disable && state.state !== 'unfixed') {
+  static getDerivedStateFromProps(props, state) {
+    if (props.disable && state.state !== "unfixed") {
       return {
         translateY: 0,
-        className: 'headroom headroom--unfixed headroom-disable-animation',
+        className: "headroom headroom--unfixed headroom-disable-animation",
         animation: false,
-        state: 'unfixed',
-      }
+        state: "unfixed",
+      };
     }
 
-    return null
+    return null;
   }
 
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     // Class variables.
-    this.currentScrollY = 0
-    this.lastKnownScrollY = 0
-    this.scrollTicking = false
-    this.resizeTicking = false
+    this.currentScrollY = 0;
+    this.lastKnownScrollY = 0;
+    this.scrollTicking = false;
+    this.resizeTicking = false;
     this.state = {
-      state: 'unfixed',
+      state: "unfixed",
       translateY: 0,
-      className: 'headroom headroom--unfixed',
-    }
+      className: "headroom headroom--unfixed",
+    };
   }
 
-  componentDidMount () {
-    this.setHeightOffset()
+  componentDidMount() {
+    this.setHeightOffset();
     if (!this.props.disable) {
-      this.props.parent().addEventListener('scroll', this.handleScroll)
+      this.props.parent().addEventListener("scroll", this.handleScroll);
 
       if (this.props.calcHeightOnResize) {
-        this.props.parent().addEventListener('resize', this.handleResize)
+        this.props.parent().addEventListener("resize", this.handleResize);
       }
     }
   }
 
-  shouldComponentUpdate (nextProps, nextState) {
+  shouldComponentUpdate(nextProps, nextState) {
     return (
       !shallowequal(this.props, nextProps) ||
       !shallowequal(this.state, nextState)
-    )
+    );
   }
 
-  componentDidUpdate (prevProps, prevState) {
+  componentDidUpdate(prevProps, prevState) {
     // If children have changed, remeasure height.
     if (prevProps.children !== this.props.children) {
-      this.setHeightOffset()
+      this.setHeightOffset();
     }
 
     // Add/remove event listeners when re-enabled/disabled
     if (!prevProps.disable && this.props.disable) {
-      this.props.parent().removeEventListener('scroll', this.handleScroll)
-      this.props.parent().removeEventListener('resize', this.handleResize)
+      this.props.parent().removeEventListener("scroll", this.handleScroll);
+      this.props.parent().removeEventListener("resize", this.handleResize);
 
-      if (prevState.state !== 'unfixed' && this.state.state === 'unfixed') {
-        this.props.onUnfix()
+      if (prevState.state !== "unfixed" && this.state.state === "unfixed") {
+        this.props.onUnfix();
       }
     } else if (prevProps.disable && !this.props.disable) {
-      this.props.parent().addEventListener('scroll', this.handleScroll)
+      this.props.parent().addEventListener("scroll", this.handleScroll);
 
       if (this.props.calcHeightOnResize) {
-        this.props.parent().addEventListener('resize', this.handleResize)
+        this.props.parent().addEventListener("resize", this.handleResize);
       }
     }
   }
 
-  componentWillUnmount () {
-    this.props.parent().removeEventListener('scroll', this.handleScroll)
-    window.removeEventListener('scroll', this.handleScroll)
-    this.props.parent().removeEventListener('resize', this.handleResize)
+  componentWillUnmount() {
+    this.props.parent().removeEventListener("scroll", this.handleScroll);
+    window.removeEventListener("scroll", this.handleScroll);
+    this.props.parent().removeEventListener("resize", this.handleResize);
   }
 
-  setRef = ref => (this.inner = ref)
+  setRef = (ref) => (this.inner = ref);
 
   setHeightOffset = () => {
     this.setState({
-      height: this.inner ? this.inner.offsetHeight : '',
-    })
-    this.resizeTicking = false
-  }
+      height: this.inner ? this.inner.offsetHeight : "",
+    });
+    this.resizeTicking = false;
+  };
 
   getScrollY = () => {
     if (this.props.parent().pageYOffset !== undefined) {
-      return this.props.parent().pageYOffset
+      return this.props.parent().pageYOffset;
     } else if (this.props.parent().scrollTop !== undefined) {
-      return this.props.parent().scrollTop
+      return this.props.parent().scrollTop;
     } else {
-      return (document.documentElement || document.body.parentNode || document.body).scrollTop
+      return (
+        document.documentElement ||
+        document.body.parentNode ||
+        document.body
+      ).scrollTop;
     }
-  }
+  };
 
-  getViewportHeight = () => (
-    window.innerHeight
-      || document.documentElement.clientHeight
-      || document.body.clientHeight
-  )
+  getViewportHeight = () =>
+    window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.body.clientHeight;
 
   getDocumentHeight = () => {
-    const body = document.body
-    const documentElement = document.documentElement
+    const body = document.body;
+    const documentElement = document.documentElement;
 
     return Math.max(
-      body.scrollHeight, documentElement.scrollHeight,
-      body.offsetHeight, documentElement.offsetHeight,
-      body.clientHeight, documentElement.clientHeight
-    )
-  }
+      body.scrollHeight,
+      documentElement.scrollHeight,
+      body.offsetHeight,
+      documentElement.offsetHeight,
+      body.clientHeight,
+      documentElement.clientHeight
+    );
+  };
 
-  getElementPhysicalHeight = elm => Math.max(
-    elm.offsetHeight,
-    elm.clientHeight
-  )
+  getElementPhysicalHeight = (elm) =>
+    Math.max(elm.offsetHeight, elm.clientHeight);
 
-  getElementHeight = elm => Math.max(
-    elm.scrollHeight,
-    elm.offsetHeight,
-    elm.clientHeight,
-  )
+  getElementHeight = (elm) =>
+    Math.max(elm.scrollHeight, elm.offsetHeight, elm.clientHeight);
 
   getScrollerPhysicalHeight = () => {
-    const parent = this.props.parent()
+    const parent = this.props.parent();
 
-    return (parent === window || parent === document.body)
+    return parent === window || parent === document.body
       ? this.getViewportHeight()
-      : this.getElementPhysicalHeight(parent)
-  }
+      : this.getElementPhysicalHeight(parent);
+  };
 
   getScrollerHeight = () => {
-    const parent = this.props.parent()
+    const parent = this.props.parent();
 
-    return (parent === window || parent === document.body)
+    return parent === window || parent === document.body
       ? this.getDocumentHeight()
-      : this.getElementHeight(parent)
-  }
+      : this.getElementHeight(parent);
+  };
 
   isOutOfBound = (currentScrollY) => {
-    const pastTop = currentScrollY < 0
+    const pastTop = currentScrollY < 0;
 
-    const scrollerPhysicalHeight = this.getScrollerPhysicalHeight()
-    const scrollerHeight = this.getScrollerHeight()
+    const scrollerPhysicalHeight = this.getScrollerPhysicalHeight();
+    const scrollerHeight = this.getScrollerHeight();
 
-    const pastBottom = currentScrollY + scrollerPhysicalHeight > scrollerHeight
+    const pastBottom = currentScrollY + scrollerPhysicalHeight > scrollerHeight;
 
-    return pastTop || pastBottom
-  }
+    return pastTop || pastBottom;
+  };
 
   handleScroll = () => {
     if (!this.scrollTicking) {
-      this.scrollTicking = true
-      raf(this.update)
+      this.scrollTicking = true;
+      raf(this.update);
     }
-  }
+  };
 
   handleResize = () => {
     if (!this.resizeTicking) {
-      this.resizeTicking = true
-      raf(this.setHeightOffset)
+      this.resizeTicking = true;
+      raf(this.setHeightOffset);
     }
-  }
+  };
 
   unpin = () => {
-    this.props.onUnpin()
+    this.props.onUnpin();
 
     this.setState({
-      translateY: '-100%',
-      className: 'headroom headroom--unpinned',
+      translateY: "-100%",
+      className: "headroom headroom--unpinned",
       animation: true,
-      state: 'unpinned',
-    })
-  }
+      state: "unpinned",
+    });
+  };
 
   unpinSnap = () => {
-    this.props.onUnpin()
+    this.props.onUnpin();
 
     this.setState({
-      translateY: '-100%',
-      className: 'headroom headroom--unpinned headroom-disable-animation',
+      translateY: "-100%",
+      className: "headroom headroom--unpinned headroom-disable-animation",
       animation: false,
-      state: 'unpinned',
-    })
-  }
+      state: "unpinned",
+    });
+  };
 
   pin = () => {
-    this.props.onPin()
+    this.props.onPin();
 
     this.setState({
       translateY: 0,
-      className: 'headroom headroom--pinned',
+      className: "headroom headroom--pinned",
       animation: true,
-      state: 'pinned',
-    })
-  }
+      state: "pinned",
+    });
+  };
 
   unfix = () => {
-    this.props.onUnfix()
+    this.props.onUnfix();
 
     this.setState({
       translateY: 0,
-      className: 'headroom headroom--unfixed headroom-disable-animation',
+      className: "headroom headroom--unfixed headroom-disable-animation",
       animation: false,
-      state: 'unfixed',
-    })
-  }
+      state: "unfixed",
+    });
+  };
 
   update = () => {
-    this.currentScrollY = this.getScrollY()
+    this.currentScrollY = this.getScrollY();
 
     if (!this.isOutOfBound(this.currentScrollY)) {
       const { action } = shouldUpdate(
@@ -253,41 +254,44 @@ export default class Headroom extends Component {
         this.currentScrollY,
         this.props,
         this.state
-      )
+      );
 
-      if (action === 'pin') {
-        this.pin()
-      } else if (action === 'unpin') {
-        this.unpin()
-      } else if (action === 'unpin-snap') {
-        this.unpinSnap()
-      } else if (action === 'unfix') {
-        this.unfix()
+      if (action === "pin") {
+        this.pin();
+      } else if (action === "unpin") {
+        this.unpin();
+      } else if (action === "unpin-snap") {
+        this.unpinSnap();
+      } else if (action === "unfix") {
+        this.unfix();
       }
     }
 
-    this.lastKnownScrollY = this.currentScrollY
-    this.scrollTicking = false
-  }
+    this.lastKnownScrollY = this.currentScrollY;
+    this.scrollTicking = false;
+  };
 
-  render () {
-    const { className: userClassName, ...divProps } = this.props
-    delete divProps.onUnpin
-    delete divProps.onPin
-    delete divProps.onUnfix
-    delete divProps.disableInlineStyles
-    delete divProps.disable
-    delete divProps.parent
-    delete divProps.children
-    delete divProps.upTolerance
-    delete divProps.downTolerance
-    delete divProps.pinStart
-    delete divProps.calcHeightOnResize
+  render() {
+    const { className: userClassName, ...divProps } = this.props;
+    delete divProps.onUnpin;
+    delete divProps.onPin;
+    delete divProps.onUnfix;
+    delete divProps.disableInlineStyles;
+    delete divProps.disable;
+    delete divProps.parent;
+    delete divProps.children;
+    delete divProps.upTolerance;
+    delete divProps.downTolerance;
+    delete divProps.pinStart;
+    delete divProps.calcHeightOnResize;
 
-    const { style, wrapperStyle, ...rest } = divProps
+    const { style, wrapperStyle, ...rest } = divProps;
 
     let innerStyle = {
-      position: this.props.disable || this.state.state === 'unfixed' ? 'relative' : 'fixed',
+      position:
+        this.props.disable || this.state.state === "unfixed"
+          ? "relative"
+          : "fixed",
       top: 0,
       left: 0,
       right: 0,
@@ -295,9 +299,9 @@ export default class Headroom extends Component {
       WebkitTransform: `translate3D(0, ${this.state.translateY}, 0)`,
       MsTransform: `translate3D(0, ${this.state.translateY}, 0)`,
       transform: `translate3D(0, ${this.state.translateY}, 0)`,
-    }
+    };
 
-    let className = this.state.className
+    let className = this.state.className;
 
     // Don't add css transitions until after we've done the initial
     // negative transform when transitioning from 'unfixed' to 'unpinned'.
@@ -306,31 +310,31 @@ export default class Headroom extends Component {
     if (this.state.animation) {
       innerStyle = {
         ...innerStyle,
-        WebkitTransition: 'all .2s ease-in-out',
-        MozTransition: 'all .2s ease-in-out',
-        OTransition: 'all .2s ease-in-out',
-        transition: 'all .2s ease-in-out',
-      }
-      className += ' headroom--scrolled'
+        WebkitTransition: "all .2s ease-in-out",
+        MozTransition: "all .2s ease-in-out",
+        OTransition: "all .2s ease-in-out",
+        transition: "all .2s ease-in-out",
+      };
+      className += " headroom--scrolled";
     }
 
     if (!this.props.disableInlineStyles) {
       innerStyle = {
         ...innerStyle,
         ...style,
-      }
+      };
     } else {
-      innerStyle = style
+      innerStyle = style;
     }
 
     const wrapperStyles = {
       ...wrapperStyle,
       height: this.state.height ? this.state.height : null,
-    }
+    };
 
     const wrapperClassName = userClassName
       ? `${userClassName} headroom-wrapper`
-      : 'headroom-wrapper'
+      : "headroom-wrapper";
 
     return (
       <div style={wrapperStyles} className={wrapperClassName}>
@@ -343,6 +347,6 @@ export default class Headroom extends Component {
           {this.props.children}
         </div>
       </div>
-    )
+    );
   }
 }
