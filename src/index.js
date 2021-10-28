c90d9136306e106035fa900a0b8b56138e82cf0c
Eduardo Oliveira <eduardo.o.carvalho@gmail.com>
2021-10-28 11:37:10 -0300

    should fix height problem for heroes

--- src/index.js
+++ src/index.js
@@ -401,7 +401,7 @@ export default class Headroom extends Component {
 
     const wrapperStyles = {
       ...wrapperStyle,
-      height: this.state.height ? this.state.height : null,
+      // height: this.state.height ? this.state.height : null,
     }
 
     const wrapperClassName = userClassName
